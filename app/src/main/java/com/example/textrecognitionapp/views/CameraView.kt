56a786e10f9a874f1ac67a4d7cbd3f3cf393be49
Jamiro <jamirocristian15@gmail.com>
2023-10-03 17:00:20 -0300

    adding name to photo

--- app/src/main/java/com/example/textrecognitionapp/views/CameraView.kt
+++ app/src/main/java/com/example/textrecognitionapp/views/CameraView.kt
@@ -1,10 +1,104 @@
 package com.example.textrecognitionapp.views
 
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
+import coil.compose.rememberAsyncImagePainter
+import com.example.textrecognitionapp.R
 import com.example.textrecognitionapp.viewModel.ScannerViewModel
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 @Composable
 fun CameraView(viewModel: ScannerViewModel) {
-    Text(text = "Camera")
+    val context = LocalContext.current
+    val clipboard = LocalClipboardManager.current
+
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        context.packageName + ".provider", file
+    )
+
+    var image: Any? by remember { mutableStateOf(R.drawable.ic_gallery) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            image = it
+            viewModel.onRecognizedText(image, context)
+        } else {
+            viewModel.showToast(context, "no image selected")
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+                .padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        val scrollState = rememberScrollState()
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipboard.setText(AnnotatedString(viewModel.recognizedText))
+                    viewModel.showToast(context, "Copy")
+                }
+        )
+    }
 }
+@SuppressLint("SimpleDateFormat")
+fun Context.createImageFile(): File {
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    return File.createTempFile(
+        imageFileName,
+        ".jpg",
+        externalCacheDir
+    )
+}
+
