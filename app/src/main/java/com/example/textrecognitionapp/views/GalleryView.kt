d5f6a00b2baa9bcf75b67e0b827be217c897dc79
Jamiro <jamirocristian15@gmail.com>
2023-10-03 16:43:08 -0300

    get text from images1

--- app/src/main/java/com/example/textrecognitionapp/views/GalleryView.kt
+++ app/src/main/java/com/example/textrecognitionapp/views/GalleryView.kt
@@ -6,8 +6,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.textrecognitionapp.R
@@ -22,14 +30,16 @@ import com.example.textrecognitionapp.viewModel.ScannerViewModel
 
 @Composable
 fun GalleryView(viewModel: ScannerViewModel) {
+    val context = LocalContext.current
     var image: Any? by remember { mutableStateOf(R.drawable.ic_gallery) }
     val photoPicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia()
     ) {
         if (it != null) {
             image = it
+            viewModel.onRecognizedText(image, context)
         } else {
-            //Toast
+            viewModel.showToast(context, "no image selected")
         }
     }
     Column(
@@ -49,6 +59,14 @@ fun GalleryView(viewModel: ScannerViewModel) {
                 .padding(16.dp, 8.dp),
             painter = rememberAsyncImagePainter(image),
             contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        val scrollState = rememberScrollState()
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+                .verticalScroll(scrollState)
         )
     }
 
