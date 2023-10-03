ba20a286a853a0c7ed0cbc35fc2c9d8925138e9c
Jamiro <jamirocristian15@gmail.com>
2023-10-03 20:47:36 -0300

    adding grid for collection using flow

--- app/src/main/java/com/example/textrecognitionapp/views/CollectionGalleryView.kt
+++ app/src/main/java/com/example/textrecognitionapp/views/CollectionGalleryView.kt
@@ -1,9 +1,72 @@
 package com.example.textrecognitionapp.views
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.textrecognitionapp.R
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CollectionGalleyView() {
-    Text(text = "Collection")
+    val context = LocalContext.current
+    var imagesUris by remember { mutableStateOf<List<Uri>>(emptyList()) }
+    val multiplePhoto = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 10)
+    ) {
+        imagesUris = it
+    }
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                multiplePhoto.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }) {
+                Icon(painter = painterResource(id = R.drawable.ic_photo), contentDescription = null)
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            FlowRow {
+                imagesUris.forEach { uri ->
+                    AsyncImage(
+                        model = ImageRequest.Builder(context).data(uri)
+                            .crossfade(enable = true).build(),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(120.dp)
+                            .padding(start = 5.dp, end = 5.dp, top = 10.dp)
+                    )
+                }
+            }
+        }
+    }
 }
