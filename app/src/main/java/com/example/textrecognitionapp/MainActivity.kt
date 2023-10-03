df3f6c5ede285af3991caa330ccda756b973130d
Jamiro <jamirocristian15@gmail.com>
2023-10-03 03:11:52 -0300

    .

--- app/src/main/java/com/example/textrecognitionapp/MainActivity.kt
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.textrecognitionapp.ui.theme.TextRecognitionAppTheme
 import com.example.textrecognitionapp.viewModel.ScannerViewModel
 import com.example.textrecognitionapp.views.TabsView
