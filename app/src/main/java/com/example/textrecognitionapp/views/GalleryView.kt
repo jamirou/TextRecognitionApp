19ac9043ccd60cb592f29356966fb0371f491e4d
Jamiro <jamirocristian15@gmail.com>
2023-10-03 03:30:45 -0300

    Add Open gallery

--- app/src/main/java/com/example/textrecognitionapp/views/GalleryView.kt
+++ app/src/main/java/com/example/textrecognitionapp/views/GalleryView.kt
@@ -1,10 +1,56 @@
 package com.example.textrecognitionapp.views
 
-import androidx.compose.material3.Text
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.textrecognitionapp.R
 import com.example.textrecognitionapp.viewModel.ScannerViewModel
 
 @Composable
 fun GalleryView(viewModel: ScannerViewModel) {
-    Text(text = "Gallery")
+    var image: Any? by remember { mutableStateOf(R.drawable.ic_gallery) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            image = it
+        } else {
+            //Toast
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+                .padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
+    }
+
+
 }
